213ab6f2d7aebfeea18d52e489c43fe30bb0dbe6
LeeWeed091205 <legiahuy912005@gmail.com>
2026-05-02 13:36:07 +0700

    FIX .env

--- backend/src/main/java/com/project2/demo/Project2Application.java
+++ backend/src/main/java/com/project2/demo/Project2Application.java
@@ -3,10 +3,24 @@ package com.project2.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class Project2Application {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+        .ignoreIfMissing()
+        .load();
+
+		System.setProperty("CLOUDINARY_NAME", dotenv.get("CLOUDINARY_NAME"));
+		System.setProperty("CLOUDINARY_KEY", dotenv.get("CLOUDINARY_KEY"));
+		System.setProperty("CLOUDINARY_SECRET", dotenv.get("CLOUDINARY_SECRET"));
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(Project2Application.class, args);
 	}
 
